e4f238690e7b8a452d28ee4d3be7c25fa3b15ba6
Kaleus <1766789972@qq.com>
2025-06-23 21:08:04 +0800

    feat: 添加单文件组件示例，包括App、School和Student组件

---
+++ vue_learning/19_单文件组件/School.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h2>学校地址:{{ address }}</h2>
+		<h2>学校名称:{{ name }}</h2>
+		<button @click="showName">提示</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'School',
+	data() {
+		return {
+			name: 'B站',
+			address: '上海',
+		};
+	},
+	methods: {
+		showName() {
+			alert(this.name);
+		},
+	},
+};
+</script>
+
+<style></style>
